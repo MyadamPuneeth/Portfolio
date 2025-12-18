79371e8ea94d064efa2e465d5f86109329cdcf0a
PuneethMyadam <PuneethMyadam@users.noreply.github.com>
2025-12-18 09:46:33 +0530

    added visit counter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import fire from '../assets/fire (1).png';
+import SupabaseConn from './SupabaseConn';
 
 const Navbar = () => {
     return (
         <div className='z-0'>
             <div className="navbar bg-black h-20 shadow-sm flex items-center justify-end pr-20 gap-2">
-                <a className="btn btn-ghost mt-2.5 font-ubuntu text-white text-xl">1028</a>
+                <a className="btn btn-ghost mt-2.5 font-ubuntu text-white text-xl"><SupabaseConn/></a>
                 <img className="w-10 h-10" src={fire}></img>
             </div>
         </div>
