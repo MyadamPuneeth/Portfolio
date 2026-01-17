6e995eac1fcee575b85775134a924773854e0000
PuneethMyadam <PuneethMyadam@users.noreply.github.com>
2026-01-17 18:27:12 +0530

    Added dp-700

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import fire from '../assets/fire (1).png';
-import SupabaseConn from './SupabaseConn';
 
 const Navbar = () => {
     return (
         <div className='z-0'>
             <div className="navbar bg-black h-20 shadow-sm flex items-center justify-end pr-20 gap-2">
-                <a className="btn btn-ghost mt-2.5 font-ubuntu text-white text-xl"><SupabaseConn/></a>
+                <a className="btn btn-ghost mt-2.5 font-ubuntu text-white text-xl">Feature build in process..</a>
                 <img className="w-10 h-10" src={fire}></img>
             </div>
         </div>
--- src/components/pages/Certifications.jsx
+++ src/components/pages/Certifications.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
+import DP700 from '../../assets/DP-700.pdf'
+import AZ900 from '../../assets/AZ-900.pdf'
 
 const Certifications = () => {
   return (
     <div className='flex flex-col items-center mt-5 w-full gap-20 h-[44vh] overflow-y-scroll scrollbar-hide'>
       <div className='flex flex-col gap-10'>
         <div className='border-2 rounded-md py-5 px-32 text-center'>
-          Effective Problem Solving and Decision making: University of California, Irvine
-          <a href="https://coursera.org/verify/UF95BWQWXJD7"> (<u>creds</u>)</a>
-        </div>
-        <div className='border-2 rounded-md py-5 px-32 text-center'>
-          Data Structures, University of California, San Diego: University of California, San Diego
-          <a href="https://coursera.org/verify/C8N3YUD6RH9R"> (<u>creds</u>)</a>
-        </div>
-        <div className='border-2 rounded-md py-5 px-32 text-center'>
-          Python Data Structures: University of Michigan
-          <a href="https://coursera.org/verify/YNTJUUWBNUJ8"> (<u>creds</u>)</a>
+          DP700 - Microsoft Certified Fabric Data Engineer Associate.
+          <a href={DP700} target="_blank" rel="noreferrer"> (<u>View</u>)</a>
         </div>
         <div className='border-2 rounded-md py-5 px-32 text-center'>
           AZ900 - Microsoft Azure Fundamentals
+          <a href={AZ900} target="_blank" rel="noreferrer"> (<u>View</u>)</a>
         </div>
         <div className='border-2 rounded-md py-5 px-32 text-center'>
+          Effective Problem Solving and Decision making: University of California, Irvine
+          <a href="https://coursera.org/verify/UF95BWQWXJD7"> (<u>View</u>)</a>
+        </div>
+        <div className='border-2 rounded-md py-5 px-32 text-center'>
+          Data Structures, University of California, San Diego: University of California, San Diego
+          <a href="https://coursera.org/verify/C8N3YUD6RH9R"> (<u>View</u>)</a>
+        </div>
+        <div className='border-2 rounded-md py-5 px-32 text-center'>
+          Python Data Structures: University of Michigan
+          <a href="https://coursera.org/verify/YNTJUUWBNUJ8"> (<u>View</u>)</a>
+        </div>        
+        <div className='border-2 rounded-md py-5 px-32 text-center'>
           Cryptography: University of Maryland
-          <a href="https://www.coursera.org/account/accomplishments/verify/QJL4WX6YN325"> (<u>creds</u>)</a>
+          <a href="https://www.coursera.org/account/accomplishments/verify/QJL4WX6YN325"> (<u>View</u>)</a>
         </div>
       </div>
     </div>
